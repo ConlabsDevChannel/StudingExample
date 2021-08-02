baa878558a5d31a11cad8b4d0edc6955d56c77fb
ConLabs: Develop Channel <shud8745@gmail.com>
2021-08-02 20:41:14 +0900

    21.08.02 Thread Join 걸어 종료하도록 변경. Live Streaming 종료 후 결과 수정

--- sources/dev/conlabs/sample/thread/SubThread.java
+++ sources/dev/conlabs/sample/thread/SubThread.java
@@ -2,24 +2,29 @@ package dev.conlabs.sample.thread;
 
 public class SubThread extends Thread {
 
-	// 각 Thread에서 플래그를 줘서 메인이 주기적으로 체크를 하게 만들고
-	public boolean keepGoing = true;
-	
 	@Override
 	public void run() {
-		System.out.println(this.getName() + " 시작");
+		int i = 0;
 		
-		try {
-			// 3초 뒤에 플래그를 false로 처리
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		while(true) {
+			
+			// 1초단위로 Sleep
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			i++;
+			
+			System.out.println(this.getName() + " " + i + " 초 경과");
+			
+			// i가 3 이상이면 while문 종료
+			if(i >= 3) {
+				break;
+			}
 		}
 		
-		// 거기서 둘다 상태가 종료가 나면
-		keepGoing = false;
-		System.out.println("keepGoing Set " + keepGoing);
-		System.out.println(this.getName() + " 종료");
+		// 반복문이 종료되었으니 Thread 종료
 	}
 	
 }
